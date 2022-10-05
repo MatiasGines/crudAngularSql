2226126ababaabc88a5cfc1bd20f476b31f2114a
MatiasGines <matiasgines47@gmail.com>
2022-10-04 21:53:18 -0300

    bind campos
    bindeamos los campos del formulario con reactive forms module (formularios reactivos)

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-tarjeta-credito',
   templateUrl: './tarjeta-credito.component.html',
   styleUrls: ['./tarjeta-credito.component.css']
 })
+
+
 export class TarjetaCreditoComponent implements OnInit {
   listTarjetas: any[] = [
     {
@@ -15,18 +19,31 @@ export class TarjetaCreditoComponent implements OnInit {
     }
   ];
 
-  form: formGroup;
-  constructor(private fb: formBuilder) {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder){
     this.form = this.fb.group({
       titular: [''],
       numeroTarjeta: [''],
       fechaExpiracion: [''],
-      cvv:['']
-    }) 
-
+      cvv: ['']
+    })
   }
 
   ngOnInit(): void {
+  }
+
+  agregarTarjeta(){
+    //obtener los valores en un objeto
+    const tarjeta: any = {
+    titular: this.form.get('titular')?.value,
+    numeroTarjeta: this.form.get('titular')?.value,
+    fechaExpiracion: this.form.get('titular')?.value,
+    cvv: this.form.get('titular')?.value
+    }
+    
+    this.listTarjetas.push(tarjeta);
+    this.form.reset();
   }
 
 }
